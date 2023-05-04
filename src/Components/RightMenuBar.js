57265697e75c6f8578af3132b14a8397268f48be
akshay <akshay.singhal@neosoftmail.com>
2023-05-04 14:55:23 +0530

    branch

--- src/Components/RightMenuBar.js
+++ src/Components/RightMenuBar.js
@@ -37,7 +37,7 @@ function RightMenuBar() {
 
   return (
     <RightWrapper>
-      <TitleWrapper>Gig Execution Module with Docker jenkins new 123</TitleWrapper>
+      <TitleWrapper>Gig Execution Module with Docker jenkins new 123 jenkins</TitleWrapper>
       <SubHeading>Allocate Gigs, Track Execution, Track Execution by Giggers</SubHeading>
       <MenuWrapper>
         {
